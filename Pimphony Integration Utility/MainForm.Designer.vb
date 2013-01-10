572a0e8d27e76a451e6f29de188dfda99102fea0
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2013-01-11 06:24:00 +0700

    just an icon

--- Pimphony Integration Utility/MainForm.Designer.vb
+++ Pimphony Integration Utility/MainForm.Designer.vb
@@ -22,6 +22,7 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
@@ -147,6 +148,7 @@ Partial Class MainForm
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "MainForm"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Pimphony Integration Utility"

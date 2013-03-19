fbe70e5d9ab21363754fe28bc7cae5f2e9390c50
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2013-03-19 11:53:09 +0700

    attempting using autoupdate
    still not correct

--- DesktopNotifier/Program.cs
+++ DesktopNotifier/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using DesktopNotifier.Models;
+using System.Xml;
+using System.Net;
+using NAppUpdate.Framework;
+using NAppUpdate.Framework.Common;
+using NAppUpdate.Framework.Sources;
 
 namespace DesktopNotifier
 {
@@ -22,9 +27,91 @@ namespace DesktopNotifier
         public static StaffModel loginStaff = null;
         public static List<BulletinModel> listBulletin = new List<BulletinModel>();
         public static MainForm mainForm;
+
+        #region autoupdate
+        private static void OnPrepareUpdatesCompleted(IAsyncResult asyncResult)
+        {
+            try
+            {
+                ((UpdateProcessAsyncResult)asyncResult).EndInvoke();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Updates preparation failed. Check the feed and try again.{0}{1}", Environment.NewLine, ex));
+                return;
+            }
+
+            // Get a local pointer to the UpdateManager instance
+            UpdateManager updManager = UpdateManager.Instance;
+
+            DialogResult dr = MessageBox.Show("Updates are ready to install. Do you wish to install them now?", "Software updates ready", MessageBoxButtons.YesNo);
+
+            if (dr != DialogResult.Yes) return;
+            // This is a synchronous method by design, make sure to save all user work before calling
+            // it as it might restart your application
+            try
+            {
+                updManager.ApplyUpdates(true);//chkRelaunch.Checked, chkLogging.Checked, chkShowConsole.Checked);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Error while trying to install software updates{0}{1}", Environment.NewLine, ex));
+            }
+        }
+
+        private static void CheckForUpdates(IUpdateSource source)
+        {
+            // Get a local pointer to the UpdateManager instance
+            UpdateManager updManager = UpdateManager.Instance;
+            updManager.ReinstateIfRestarted(); // required to be able to restore state after app restart
+            updManager.Config.BackupFolder = "g:\\temp\\update\\backup";
+            updManager.Config.TempFolder = "g:\\temp\\update\\temp";
+
+            // Only check for updates if we haven't done so already
+            if (updManager.State != UpdateManager.UpdateProcessState.NotChecked)
+            {
+                MessageBox.Show("Update process has already initialized; current state: " + updManager.State.ToString());
+                return;
+            }
+
+            try
+            {
+                // Check for updates - returns true if relevant updates are found (after processing all the tasks and
+                // conditions)
+                // Throws exceptions in case of bad arguments or unexpected results
+                updManager.CheckForUpdates(source);
+            }
+            catch (Exception ex)
+            {
+                if (ex is NAppUpdateException)
+                {
+                    // This indicates a feed or network error; ex will contain all the info necessary
+                    // to deal with that
+                }
+                else MessageBox.Show(ex.ToString());
+                return;
+            }
+
+
+            if (updManager.UpdatesAvailable == 0)
+            {
+                MessageBox.Show("Your software is up to date");
+                return;
+            }
+
+            DialogResult dr = MessageBox.Show(string.Format("Updates are available to your software ({0} total). Do you want to download and prepare them now? You can always do this at a later time.", updManager.UpdatesAvailable), "Software updates available", MessageBoxButtons.YesNo);
+
+            if (dr == DialogResult.Yes) updManager.BeginPrepareUpdates(OnPrepareUpdatesCompleted, null);
+        }
+        #endregion
+
+
         [STAThread]
         static void Main()
         {
+            #region autoupdate
+            CheckForUpdates(new NAppUpdate.Framework.Sources.SimpleWebSource("http://localhost/piu/piu.xml"));
+            #endregion 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             RegistrySettings.loadValues();
